8802a14767357918b90efbb29009f415c0d2326a
Mohan Gummadi <mohanaraogummadi@gmail.com>
2025-08-31 16:08:12 +0530

    Configure OpenAPI for JWT security

--- src/main/java/com/easyreach/backend/config/OpenApiConfig.java
+++ src/main/java/com/easyreach/backend/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.easyreach.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(new Info().title("Easyreach API").version("v1"));
+        final String schemeName = "bearerAuth";
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info().title("Easyreach API").version("v1"))
+                .components(new Components().addSecuritySchemes(schemeName, securityScheme))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
     }
 }
