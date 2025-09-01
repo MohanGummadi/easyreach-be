0d35dfde714c66d3aa77f405a4cc081d84923d94
Mohan Gummadi <mohanaraogummadi@gmail.com>
2025-09-01 10:43:41 +0530

    Add mobile number uniqueness constraint and checks

--- src/main/java/com/easyreach/backend/repository/UserRepository.java
+++ src/main/java/com/easyreach/backend/repository/UserRepository.java
@@ -13,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmailIgnoreCase(String email);
     Optional<User> findByEmailIgnoreCaseAndCompanyUuid(String email, String companyUuid);
     Optional<User> findByMobileNo(String mobileNo);
+    boolean existsByMobileNo(String mobileNo);
 
     List<User> findByCompanyUuidAndUpdatedAtGreaterThanEqual(String companyUuid, OffsetDateTime cursor, Pageable pageable);
 
---
+++ src/main/resources/db/migration/V2__add_unique_constraint_on_users_mobile_no.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.users
+    ADD CONSTRAINT users_mobile_no_key UNIQUE (mobile_no);
