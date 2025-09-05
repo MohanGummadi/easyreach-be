98b4329435aa34204fe34434578255a019b11b4d
Moha Gummadi <mohanaraogummadi@gmail.com>
2025-09-05 16:27:08 +0530

    Fixed test changes

--- src/main/java/com/easyreach/backend/service/impl/CompanyScopedService.java
+++ src/main/java/com/easyreach/backend/service/impl/CompanyScopedService.java
@@ -2,18 +2,18 @@ package com.easyreach.backend.service.impl;
 
 import com.easyreach.backend.security.CompanyContext;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 public abstract class CompanyScopedService {
     protected String currentCompany() {
         log.debug("Entering currentCompany");
-        String companyUuid = CompanyContext.getCompanyId();
-        if (companyUuid == null) {
+        String companyId = CompanyContext.getCompanyId();
+        if (!StringUtils.hasText(companyId)) {
             log.error("Company ID is not set in context");
             throw new IllegalStateException("Company ID is not set in context");
         }
-        log.debug("Exiting currentCompany with companyUuid={}", companyUuid);
-        return companyUuid;
+        log.debug("Exiting currentCompany with companyId={}", companyId);
+        return companyId;
     }
 }
-
